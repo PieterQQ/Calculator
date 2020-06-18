8a4a986361a50140bbf53b53af07e0d7af8fc4e9
PieterQQ <pieter444@wp.pl>
2020-06-18 11:05:09 +0200

    Numbers Works

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -15,14 +15,34 @@ using System.Windows.Shapes;
 
 namespace Calculator
 {
+   
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private decimal number1 = 0;
+        private decimal number2 = 0;
+        private string operation = null;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NumberBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            if (operation is null)
+            {
+               
+                number1 = (number1 * 10) + decimal.Parse(clickedButton.Tag.ToString());
+                Display.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + (decimal)clickedButton.Tag;
+                Display.Text = number2.ToString();
+            }
         }
     }
 }
